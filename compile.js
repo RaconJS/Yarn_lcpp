719ab3684ee1a6746b9fc1d35cd919958f0f0547
racon <46864506+RaconJS@users.noreply.github.com>
2023-04-23 21:54:05 +0100

    attempt of parsing assignments

--- compile.js
+++ compile.js
@@ -108,6 +108,11 @@
 				return Object.assign(new Lazy(...this),{context,recur}).eval();
 			}
 		}
+		class Assignment{
+			constructor(labelName,value,parent){//:(String,ContextTree) 'a = 2'
+				Object.assign(this,{labelName,value,parent});
+			}
+		}
 		class RecurSetter extends Exp{//:Exp
 			constructor(exp,recur){
 				Object.assign(this,{
@@ -120,7 +125,7 @@
 					Math.max(findHighestContext(this.exp),findHighestContext(this.recur));
 			}
 		}
-		class NameSpace extends Exp{
+		class NameSpace extends Exp{//'{a b c}' and '{a<=1,b<=2,c<=3}'
 			constructor(labels,lazy){
 				this.labels = labels;
 				this.lazy = lazy;
@@ -206,33 +211,170 @@
 //----
 function loga(...logs){console.log(...logs)}
 function compile (text){
-	function treeParse(words,index){
+	//types
+	function throwError(wordNum,type,errorMessage,error){//error = str=>Error(str)
+		let data = getLineFromWord.get(wordNum);
+		let line = lines[data.line-1];
+		let whiteSpace = line.match(/^[\t ]*/)?.[0]??"";
+		line = line.substr(whiteSpace.length)//[whiteSpace,line]
+		throw error(
+			"l:"+data.line+" c:"+data.column+"\n"
+			+line+"\n"
+			+" ".repeat(data.column-1-whiteSpace.length)+"^"+"\n"
+			+type+" error"+": "+errorMessage+"\n"
+		);
+	}
+	function treeParse(words){
 		let list = [];
-		let value;
-		let done;
-		while(!done){
-			{
-				value = iter.next();
-				done = value.done;
+		let stack = [];
+		for(let i=0;i<words.length;i++){
+			let word = words[i];
+			let value = [word,i];
+			if(word.match(/[(\[{]/)){
+				stack.push(list);
+				list.push(value,list = []);
 			}
-			if(value[0].match(/[(\[{]/)){
-
+			else if(word.match(/[)\]}]/)){
+				list = stack.pop();
+				if(!list)throwError(i,"syntax", "unballanced brackets: Too many closing brackets", a=>Error(a));
+				//assert: list[list.length-2] exists
+				let openBracket = list[list.length-2][0];
+				if(({"(":")", "[":"]", "{":"}"})[openBracket]!=word)throwError(i,"syntax", "unmatching brackets:'"+openBracket+"..."+word+"'", a=>Error(a));
+				list.push(value);
 			}
-
+			else list.push(value);
 		}
+		if(stack.length>0)throwError(i,"syntax", "unballanced brackets: Too many opening brackets", a=>Error(a));
 		return list;
 	}
-	function parse(context=[]){
-		let list = [];
-		let startList = list;
-		let startContext = context;
-		let word = iter.next(),value1;
-		for(let i = 0;i<text.length&&!word.done;i++){
-			let value = word.value;
-			word= value[0];
+	function parseContexts(tree){
+		let list = new Lazy();//:list of words
+		let context = {finalMap:new Map()};//:Map<String -> Assignment>
+		let i=0;
+		const getState = ()=>({tree,context,list,i});
+		const setState = v=>({tree,context,list,i}=v);
+		const stack = [{tree,context,i:0,list}];
+		const getWord = i=>tree[i]?.[0]??"";
+		class Pattern{
+			param;//:(String & word) | Tree<String>
+			pattern;//:String? & word
+			options;//:String & joined words
+		}
+		class bracketPattern extends Pattern{
+			pattern;//')'|']'|'}'
+			list;//:Tree<Pattern> ; brackets only
+		}
+		const match_pattern = i=>{//Number->String
+			let pattern = "";
+			let options="";//'()=' '()<=>'
+			let param;//:Tree|[String,Number]
+			{//assign param
+				if("([{".includes(getWord(i))){
+					options+=getWord(i)+getWord(i+2);
+					param = tree[i+1];
+					i+=3;
+				}
+				else param = tree[i++];
+			}
+			if(getWord(i)=="?"){options+="?";++i}
+			if(getWord(i)==">"){options+=">";++i;}
+			else {
+				if(getWord(i)=="<"){options+="<";++i}
+				if(getWord(i)=="="){pattern="=";options+="=";++i}
+				if(getWord(i)==">"){options+=">";++i}
+			}
+			return {param,pattern,options,i};//`i-1` is to negate the `i++` from the for loop
+		};
+		const removeIndents=num=>{//removes indentation from strings '""'
+			let word = tree[num];
+			if(!word.match("\n"))return word;
+			let indentNum = (word.match(/\n\s*/g)??[]).reduce((s,v)=>Math.min(s,v.length),Infinity);
+			return word.split("\n").reduce((s,v)=>s+v.substr(indentNum));
+		};
+		for(let i1 = 0; i1<words.length; i1++){
+			if(i>=tree.length){
+				setState(stack.pop());//`{i} = stack` with `i++` skips the end bracket
+				continue;
+			}//closeBracket
+			let value = tree[i];
+			let [word,num] = value;
+			let match;
+			if((match=match_pattern(i+1)).pattern){
+				({i}=match);
+				list.push(match);
+			}
+			else if("([{".includes(word)){
+				stack.push(getState());
+				context = [];
+				tree = tree[i+1];
+				i=-1;//next i == 0
+			}
+			else if(")]}".includes(word)){
+				stack[stack.length-1].list.push({pattern:word,list})
+				setState(stack.pop());
+				//eval labels
+			}
+			else if(word == ","){({context} = stack[stack.length-1])}
+			else {}
+		}
+		if(false)for(let i1 = 0; i1<words.length; [i1++,i++]){
+			if(i>=tree.length){
+				setState(stack.pop());//`{i} = stack` with `i++` skips the end bracket
+				continue;
+			}//closeBracket
+			let value = tree[i];
+			let [word,num] = value;
+			let match;
+			if((match=match_pattern(i+1)).pattern){
+				({i}=match);
+				if(match.pattern == ">"){
+					context = [match.param,...context];
+					list.push({context,match});
+				}
+				if(match.pattern == "="){
+					context = [match.param,...context];
+					list.push({context,match});
+				}
+			}
+			else if("([{".includes(word)){
+				stack.push(getState());
+				context = [];
+				tree = tree[i+1];
+				i=-1;//next i == 0
+			}
+			else if(")]}".includes(word)){
+				setState(stack.pop())
+				//eval labels
+			}
+			else if(word == ","){({context} = stack[stack.length-1])}
+			else {}
+		}
+	}
+	function parse(tree,list=new Lazy(),context=[],startI=0){
+		const startList = list;
+		for(let i = startI;i<tree.length;i++){
+			let value = tree[i];
+			let [word,num] = value;
 			if(word.match(/^\/\/|^\/\*|\s+/)){}//ignore comments & white space
-			else if(word == "("){
-				list.push(parse(context));
+			else if(word.match(/[([{]/)){
+				let i1 = i+3;
+				let isMut = tree[i1]?.[0]=="?";
+				if(isMut)i1++;
+				let word1= tree[i1]?.[0]??"";
+				if(word1.match(/=|>/)){
+					if(word1.match("=")){//with'()=' or destructor assign '[]=','{}='
+					}
+					else if(word1.match("=")){//using'()>' or destructor param '[]>','{}>'
+					}
+				}
+				else {
+					switch (word){
+						case "(":list.push(parse(tree[i+1],Object.assign(new Array(),{id:num}),context));break;
+						case "[":;break;
+						case "{":;break;
+					}
+				}
+				i+=2;
 			}
 			else if(word.match(/[)\]}]/))break;
 			else if(context.indexOf(word)!=-1){
@@ -255,25 +397,38 @@ function compile (text){
 				throw Error("recursion setting '::' is not supported")
 				//startList.push(new RecurSetter(,list=new Lazy()));
 			}
-			else if(word=="="){
-				throw Error("assignment '=' is not supported")
-				//startList.push(new RecurSetter(,list=new Lazy()));
-			}
-			else if((value1=iter.next().value)[0]==">"){
+			else if((tree[i+1]?.[0]??"")[0]==">"){
 				context = [word,...context];
-
-				list.push(list = Object.assign(new Lambda(),{id:value1}));
+				list.push(list = Object.assign(new Lambda(),{id:num}));
+				i++;
 			}
-			word = iter.next();
 		}
+		let outList = startList;
 		if(list.length == 0&&list!=startList)startList.pop();
-		if(startList.length==1)startList = startList[0];
-		return startList;
+		if(startList.length==1)outList = outList[0];
+		return outList;
 	}
 	let regex =/[λ]|\b\w+\b|"[^"]*"|[^\s]|\s+|\/\/.*|\/\*[\s\S]*\*\//g;
+	let lines = text.split("\n");
+	let words = text.match(regex);
+	const getLineFromWord=new Map();
+	const getWordFromLine=new Map();
+	words.reduce((s,v,i)=>{
+		s.word = v;
+		getLineFromWord.set(i,s);
+		getWordFromLine.set(s,i);
+		s = {...s};
+		if(v.match("\n")){
+			s.line += (v.match(/\n/g)?.length??0);
+			s.column = 1+(v.match(/.*$/g)?.length??0);
+		}
+		else s.column+=v.length;
+		return s;
+	},{line:1,column:1,word:""});
+	let tree = treeParse(words);
 	let list = text.matchAll(regex);
 	let iter = list;//list[Symbol.iterator]();
-	return new Lazy(...parse());
+	return new Lazy(...parse(tree));//new Lazy(...parse());
 }
 loga(compile(`f>(r>r x>f(r x),a>a a)`));
 //loga(Y.call(new Calc((f,n)=>n==0?1:n*f.call(n-1))).call(new Int(4)));
