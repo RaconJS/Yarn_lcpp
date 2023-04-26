33cfc1f69936e3bf70295d54ed00d3c4712b7840
racon <46864506+RaconJS@users.noreply.github.com>
2023-04-27 00:19:25 +0100

    contexts work adds errors for reference loops. links assignment and references.

--- compile.js
+++ compile.js
@@ -212,16 +212,23 @@
 function loga(...logs){console.log(...logs)}
 function compile (text){
 	//types
+		//syntax, reference
+	let TEST = false;
 	function throwError(wordNum,type,errorMessage,error){//error = str=>Error(str)
+		//note: lines and colums are counted from 1
 		let data = getLineFromWord.get(wordNum);
 		let line = lines[data.line-1];
 		let whiteSpace = line.match(/^[\t ]*/)?.[0]??"";
 		line = line.substr(whiteSpace.length)//[whiteSpace,line]
+		if(!TEST)error=a=>a;
+		let lineLen = (""+data.line).length;
 		throw error(
-			"l:"+data.line+" c:"+data.column+"\n"
-			+line+"\n"
-			+" ".repeat(data.column-1-whiteSpace.length)+"^"+"\n"
-			+type+" error"+": "+errorMessage+"\n"
+			"lc++ ERROR:\n"
+			//"l:"+data.line+" c:"+data.column+"\n"
+			// " ".repeat(lineLen)+" |\n"
+			+data.line          +" |" +line+"\n"
+			+" ".repeat(lineLen)+" |" +" ".repeat(data.column-1-whiteSpace.length)+"^".repeat(words[wordNum].length)+" "+type+" error\n"
+			+"error"+": "+errorMessage+"\n"
 		);
 	}
 	function treeParse(words){
@@ -248,107 +255,304 @@ function compile (text){
 		return list;
 	}
 	function parseContexts(tree){
-		let list = new Lazy();//:list of words
-		let context = {finalMap:new Map()};//:Map<String -> Assignment>
-		let i=0;
-		const getState = ()=>({tree,context,list,i});
-		const setState = v=>({tree,context,list,i}=v);
-		const stack = [{tree,context,i:0,list}];
-		const getWord = i=>tree[i]?.[0]??"";
-		class Pattern{
-			param;//:(String & word) | Tree<String>
-			pattern;//:String? & word
-			options;//:String & joined words
+		//classes & consts
+			const numberRegex = /^[0-9]+$/;
+			const getWord = (tree,i)=>tree[i]?.[0]??"";
+			class Pattern{
+				constructor(data){
+					Object.assign(this,data)
+					if(this.list){//if this has own context
+						Object.assign(this.list,{
+
+						})
+					}
+				}
+				id;//:[]SyntaxTree
+				type="operator";//:"operator"|"word"|"number"|"symbol"|"function"|"assignment"
+				parent;//:BracketPattern
+				isFirst;//:bool ; Is true if this is the start of a list, or can be used as the starting function.
+
+				//optionals
+					pattern;//:?String & word
+					options;//:?String & joined words
+					params;//:?(String & word) | Tree<String>
+					list;
+			}
+			class Simple extends Pattern{
+				constructor(data){super();Object.assign(this,data)}
+				//pattern:undefined;
+				set parent(v){this.#parent=v}
+				get parent(){return this.#parent}
+				#parent;
+				arg;//:String
+				value;//:Param?
+				type;
+			}
+			class BracketPattern extends Pattern{
+				constructor(data){super();Object.assign(this,data)}
+				parent;
+				pattern=')';//')'|']'|'}'
+				list=[];//:Tree<Pattern> ; brackets only
+			}
+			class Pattern_extra{
+				startLabels;//:Map(String,Ref)
+				currentLabels;//:Map(String,Ref)
+				refs;//:Set(Pattern) & Tree(Pattern)
+				refLevel;//:Number
+			};
+			class Param{
+				constructor(data){Object.assign(this,data)}
+				name;//:[String,Number] ; name of the parameter or label
+				index;//:number ; if a pattern has muli parameters it shows which one it is.
+				owner;//: Pattern & context with a {list:Array} ; is the expression that the label bellongs to
+			}
+			const comma = {};
+		//----
+		const match_pattern_arg = ({tree,i,pattern})=>{//'a' or '$1' 
+			let oldI=i;
+			let getObj = ()=>({i,id:tree[oldI][1],type:"argument"});
+			if(!pattern){
+				let word;
+				if((word=getWord(tree,i))=="$"){
+					pattern = "$";
+					++i;
+					if(getWord(tree,i).match(numberRegex)){
+						params = [tree[i]];
+						++i;
+					}
+					else {i=oldI;pattern = ""}
+					return new Pattern(getObj())
+				}
+				else if(word.match(/^\w+$/)){
+					pattern = word;
+					i++;
+					let match = new Simple({...getObj(),arg:word});
+					if(word.match(numberRegex))match.type="number";
+					return match;
+				}
+			}
+			return ;
 		}
-		class bracketPattern extends Pattern{
-			pattern;//')'|']'|'}'
-			list;//:Tree<Pattern> ; brackets only
-		}
-		const match_pattern = i=>{//Number->String
+		const match_pattern = (tree,i)=>{//Number->String
+			//note: If there's no pattern property, then no pattern was found and the object is disguarded.
+			let id = tree[i][1];
 			let pattern = "";
 			let options="";//'()=' '()<=>'
-			let param;//:Tree|[String,Number]
-			{//assign param
-				if("([{".includes(getWord(i))){
-					options+=getWord(i)+getWord(i+2);
-					param = tree[i+1];
-					i+=3;
+			let params=[];//:Tree|[String,Number]
+			let hasOwnContext=false;
+			let type="operator";
+			let oldI=i;
+			if(!pattern){
+				let word = getWord(tree,i);
+				if(word=="<"){
+					++i;
+					if(getWord(tree,i).match(/\w+/)){
+						pattern = "<";
+						params = [tree[i]];
+						++i;
+					}
+					if(getWord(tree,i)){
+
+						++i;
+					}
 				}
-				else param = tree[i++];
+				//if(getWord(tree,i+1)=="::"){} UNFINISHED
 			}
-			if(getWord(i)=="?"){options+="?";++i}
-			if(getWord(i)==">"){options+=">";++i;}
-			else {
-				if(getWord(i)=="<"){options+="<";++i}
-				if(getWord(i)=="="){pattern="=";options+="=";++i}
-				if(getWord(i)==">"){options+=">";++i}
+			if(!pattern){//function and assignment | 'with' block | 'use' block
+				i=oldI;
+				hasOwnContext=true;
+				{//assign params
+					if("([{".includes(getWord(tree,i)||undefined)){
+						options+=getWord(tree,i)+"..."+getWord(tree,i+2);
+						let forEachSyntaxTree=tree=>(foo,s0)=>{
+							let s = s0;
+							return function traverse(foo,tree){
+								for(let i=0;i<tree.length;i++){
+									if("([{".includes(tree[i][0])){
+										traverse(foo,tree[++i]);
+										i++;
+									}
+									else{
+										s=foo(tree[i],i,tree)
+									}
+								}
+							}
+						};
+						params = [];// [a b c] = 
+						tree[i+1].forEach((v,i,word)=>typeof(word=getWord(tree,i))=="string" && !"()[]{}".includes(word)?params.push(word):0);
+						i+=3;
+					}
+					else params = [tree[i++]];
+				}
+				if(getWord(tree,i)=="?"){options+="?";++i}
+				if(getWord(tree,i)==">"){options+=pattern=">";++i;type="function";}
+				else {
+					if(getWord(tree,i)=="<"){options+="<";++i}
+					if(getWord(tree,i)=="="){options+=pattern="=";++i;type="assignment";}
+					if(getWord(tree,i)==">"){options+=">";++i}
+				}
+				if(options.includes("(")&&pattern){//()> or ()=
+					pattern = "()=";//use and with blocks are counted as with blocks
+					type = "with";//{">"}[options.match(/(?<=\)).*$/)];
+				}
 			}
-			return {param,pattern,options,i};//`i-1` is to negate the `i++` from the for loop
-		};
+			if(!pattern){
+				i=oldI;
+				let match = match_pattern_arg ({tree,i});
+				if(match)return match;
+			}
+			let match = new Pattern({pattern,options,params,id,type,...(hasOwnContext?{list:[]}:{}),i});
+			if(params)match.params.forEach((v,i,a)=>a[i]=new Param({name:v,index:i,owner:match}));
+			return match;
+		}
 		const removeIndents=num=>{//removes indentation from strings '""'
 			let word = tree[num];
 			if(!word.match("\n"))return word;
 			let indentNum = (word.match(/\n\s*/g)??[]).reduce((s,v)=>Math.min(s,v.length),Infinity);
 			return word.split("\n").reduce((s,v)=>s+v.substr(indentNum));
 		};
-		for(let i1 = 0; i1<words.length; i1++){
-			if(i>=tree.length){
-				setState(stack.pop());//`{i} = stack` with `i++` skips the end bracket
-				continue;
-			}//closeBracket
-			let value = tree[i];
-			let [word,num] = value;
-			let match;
-			if((match=match_pattern(i+1)).pattern){
-				({i}=match);
-				list.push(match);
-			}
-			else if("([{".includes(word)){
-				stack.push(getState());
-				context = [];
-				tree = tree[i+1];
-				i=-1;//next i == 0
-			}
-			else if(")]}".includes(word)){
-				stack[stack.length-1].list.push({pattern:word,list})
-				setState(stack.pop());
-				//eval labels
-			}
-			else if(word == ","){({context} = stack[stack.length-1])}
-			else {}
-		}
-		if(false)for(let i1 = 0; i1<words.length; [i1++,i++]){
-			if(i>=tree.length){
-				setState(stack.pop());//`{i} = stack` with `i++` skips the end bracket
-				continue;
-			}//closeBracket
-			let value = tree[i];
-			let [word,num] = value;
-			let match;
-			if((match=match_pattern(i+1)).pattern){
-				({i}=match);
-				if(match.pattern == ">"){
-					context = [match.param,...context];
-					list.push({context,match});
+		let context = new BracketPattern;
+		let patterns = {
+			list:[...(function* getPatterns(tree,bracketContext){
+				let context = bracketContext;
+				let word;
+				let isFirst=true;
+				tree = tree.filter((v,i)=>!getWord(tree,i)?.match?.(/^(\s*|\/\*[\s\S]*\*\/|\/\/.*)$/));
+				for(let i=0;i<tree.length;isFirst=false){
+					word = getWord(tree,i);
+					let match = match_pattern(tree,i);
+					if(match.pattern||(match instanceof Simple)){
+						({i}=match);
+						match.parent = context;
+						context.list.push(match);
+						match.isFirst=isFirst;
+						if(match.list){
+							context=match;
+							isFirst=true;
+						}
+						yield match;
+						continue;
+					}
+					else if("([{".includes(getWord(tree,i))){
+						++i;
+						let bracket=new BracketPattern({pattern:"()",list:[],parent:context,isFirst});
+						yield bracket;
+						context.list.push(bracket);
+						yield*getPatterns(tree[i],bracket);
+						++i;
+					}
+					else if(")]}".includes(word)){
+						return;
+					}
+					else if(word == ","){
+						//yield comma;
+						//context.push(comma);
+						context = bracketContext;
+						isFirst=true;
+					}
+					else {
+						let match = new Simple({id:tree[i][1],arg:word,type:"symbol",parent:context,isFirst})
+						yield match;
+						context.list.push(match);
+					};
+					i++
 				}
-				if(match.pattern == "="){
-					context = [match.param,...context];
-					list.push({context,match});
+			}(tree,context))],
+			context
+		};
+		function* forEachTree(context,isTree){//isTree:node->Tree?
+			for(let i=0;i<context.list.length;i++){
+				let [v,a] = [context.list[i],context];
+				yield [v,i,a];
+				if(isTree(v))yield*forEachTree(v,isTree);
+			}
+		}
+		const forEachPattern = ()=>forEachTree(context,v=>v?.list);
+		function addRefParam(match,match_parents,param){
+			//match:Pattern
+			//match_parents:Set(BracketPattern)
+			//param:Param
+			let higherParamParent = param.owner;//:Pattern
+			let argParentIndex;
+			//assume: there is not an infinite chain of Pattern.parent's
+			while(
+				higherParamParent
+				&& -1 == (argParentIndex = match_parents.indexOf(higherParamParent.parent))
+			){higherParamParent = higherParamParent?.parent};
+			if(argParentIndex==-1)throw Error("compiler error");
+			if(!higherParamParent)throw Error("compiler error");
+			let higherArgParent = match_parents[argParentIndex-1];
+			if(!higherArgParent)throw Error("compiler error");
+			if(higherParamParent.parent!=higherArgParent.parent)throw Error("compiler error");
+			{//check for and prevent reference loops
+				if(higherParamParent.refs?.has?.(higherArgParent))throwError(match.id,"illegal reference","recursive/self reference, detected using label '"+match.arg+"'. These are not allowed",a=>Error(a))
+			}
+			//note:
+				//From now on only things from block 'arg' can reference block 'param'
+				//and never the other way around.
+				//Otherwise a reference error is thrown.
+			//----
+			//adds new reference.
+			if(!higherArgParent.refs){
+				higherArgParent.refs=new Set;//:Set(Pattern)
+				higherArgParent.refLevel??=(higherParamParent.refLevel??-1)+1;
+				higherParamParent.refLevel??=higherArgParent.refLevel-1;
+			}
+			if(higherArgParent.refLevel<=higherParamParent.refLevel)throwError(match.id,"illegal reference","complex recursive/self reference, detected using label '"+match.arg+"'. These are not allowed",a=>Error(a));
+			higherArgParent.refs.add(higherParamParent);
+		}
+		function getParam(name,parent,parents){//parents:Pattern[]?
+			let param = 
+				parent?.currentLabels?.get?.(name)
+				??parent?.startLabels?.get?.(name)
+			;
+
+			//loga(match.parent.parent.startLabels.get("a"))
+			parents?.push?.(parent);
+			return param?
+				{param,parents}:
+				parent.parent?
+					getParam(name,parent.parent,parents)
+					:undefined
+			;
+		}
+		(function getAssignments(patterns){
+			for(let [match,i,bracketParent] of forEachPattern()){
+				let lastValue = match.isFirst?undefined:bracketParent[i-1];// ','
+				if(match.pattern == "="){//assignment
+					//assume: bracketParent == match.parent
+					let startLabels=bracketParent.startLabels;
+					if(!startLabels){//set up assignment properties
+						bracketParent.startLabels = startLabels = new Map();//String|Number => Param
+						bracketParent.currentLabels = new Map();
+						bracketParent.refs = new Set();//:Set(BracketPattern) where p.refs.get(x).refs.get(p) == undefined
+					}
+					let i=0;
+					for(let param of match.params){//param:Param
+						if(!startLabels.has(param))startLabels.set(param.name[0],param);
+					}
 				}
 			}
-			else if("([{".includes(word)){
-				stack.push(getState());
-				context = [];
-				tree = tree[i+1];
-				i=-1;//next i == 0
+			for(let [match,i,bracketParent] of forEachPattern()){
+				let lastValue = match.isFirst?undefined:bracketParent[i-1];// ','
+				if(match.pattern == "="){//assignment
+					for(let param of match.params){
+						match.parent.currentLabels.set(param.name[0],param);
+					}
+				}
+				else if(match.type == "argument"){
+					if(match.pattern == "$"){}
+					else{
+						let {param,parents} = getParam(match.arg,match.parent,[])??{};
+						if(!param)throwError(match.id,"reference","label: '"+match.arg+"' is undefined",a=>Error(a));
+						addRefParam(match,parents,param);
+						//if(parent)
+						match.value = param;
+					}
+				}
 			}
-			else if(")]}".includes(word)){
-				setState(stack.pop())
-				//eval labels
-			}
-			else if(word == ","){({context} = stack[stack.length-1])}
-			else {}
-		}
+		}(patterns.list));
+		return context;
 	}
 	function parse(tree,list=new Lazy(),context=[],startI=0){
 		const startList = list;
@@ -410,7 +614,7 @@ function compile (text){
 	}
 	let regex =/[λ]|\b\w+\b|"[^"]*"|[^\s]|\s+|\/\/.*|\/\*[\s\S]*\*\//g;
 	let lines = text.split("\n");
-	let words = text.match(regex);
+	const words = text.match(regex);
 	const getLineFromWord=new Map();
 	const getWordFromLine=new Map();
 	words.reduce((s,v,i)=>{
@@ -420,15 +624,19 @@ function compile (text){
 		s = {...s};
 		if(v.match("\n")){
 			s.line += (v.match(/\n/g)?.length??0);
-			s.column = 1+(v.match(/.*$/g)?.length??0);
+			s.column = (v.match(/.*$/g)?.length??0);//assume: column >= 1
 		}
 		else s.column+=v.length;
 		return s;
 	},{line:1,column:1,word:""});
-	let tree = treeParse(words);
-	let list = text.matchAll(regex);
-	let iter = list;//list[Symbol.iterator]();
-	return new Lazy(...parse(tree));//new Lazy(...parse());
+	try{//main
+		const tree = treeParse(words);
+		const context = parseContexts(tree);
+		return context;//parse(tree);
+	}catch(error){
+		console.log(error);
+	}
 }
-loga(compile(`f>(r>r x>f(r x),a>a a)`));
+//loga
+(compile(`a=b,b=c,c=d,d=a`))//f>(r>r x>f(r x),a>a a)`));
 //loga(Y.call(new Calc((f,n)=>n==0?1:n*f.call(n-1))).call(new Int(4)));
