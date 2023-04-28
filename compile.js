a5e898894e2e0dbd724a3d015ffce1f7c65572a9
racon <46864506+RaconJS@users.noreply.github.com>
2023-04-28 01:47:21 +0100

    compiler mostly working

--- compile.js
+++ compile.js
@@ -1,3 +1,6 @@
+//TEST
+//UNTESTED
+//TODO
 //classes
 	//Exp: Lambda'λ' | Lazy'()' | Recur'::'
 	//OutExp: Lazy
@@ -21,12 +24,14 @@
 			Object.assign(this,exps);
 		}
 		call(arg=undefined,context=undefined,stack=[]){//(arg:Lazy,Stack)->Lazy
+			//let v = this;
+			//if(v!=this)v.call(arg,this.context,stack);
 			if(this.length == 1 && this[0] instanceof Lambda)return this[0].call(arg,this.context,stack);
 			return this.eval(stack).call(arg);
 		}//(a>a)(a>a a)
 		eval(stack=[]){
 			const context = this.context;
-			return this.map(v=>//[]->{call:(arg:Lazy,Stack)->Combinator|Lazy&{call:(Exp,Context,Stack)->Lazy}}[]
+			let ans = this.map(v=>//[]->{call:(arg:Lazy,Stack)->Combinator|Lazy&{call:(Exp,Context,Stack)->Lazy}}[]
 				typeof v == "number"?context.getValue(v):
 				typeof v == "string"?v://simple raw string cannot be called
 				//typeof v == "string"?:
@@ -48,8 +53,14 @@
 			).reduce((s,v)=>
 				s.call(v,context,stack)
 			)
+			if(ans instanceof Lazy)
+			while(ans.length == 1 && ans[0] instanceof Lazy)ans=ans[0];//assume: is Tree
+			return ans;
 		}
 		toInt(foo){//foo:{call(inc)->{call(0)->Number}}
+			return this.constructor.toInt(foo);
+		}
+		static toInt(foo){//foo:{call(inc)->{call(0)->Number}}
 			if(foo instanceof Int)return foo;
 			const inc = x=>x instanceof Int?new Int(x+1):new Lazy(Int.increment)
 			const zero = new Int(0);
@@ -107,6 +118,10 @@
 				let recur = arg instanceof Lazy?arg.recur:1;
 				return Object.assign(new Lazy(...this),{context,recur}).eval();
 			}
+			eval(){return this}
+			toJS(){
+				return function(arg){return this.call(arg)}.bind(this);
+			}
 		}
 		class Assignment{
 			constructor(labelName,value,parent){//:(String,ContextTree) 'a = 2'
@@ -126,22 +141,27 @@
 			}
 		}
 		class NameSpace extends Exp{//'{a b c}' and '{a<=1,b<=2,c<=3}'
-			constructor(labels,lazy){
-				this.labels = labels;
+			constructor(labels,lazy){//labels:json-like object
+				this.labels = new Map(...function*(obj){for(let i in obj)yield [i,obj[i]]}(labels));
 				this.lazy = lazy;
 			}
-			labels;//labels:{[String]:Lazy}
+			labels;//labels:Map([String],Lazy)
 			lazy;//:lazy ()
 			call(arg,context,stack){
 				if(this.lazy)return this.lazy.call(arg,context,stack);//{a>a a} x -> (a>a a) x
 				if(arg instanceof NameSpace)return new NameSpace({...this.labels,...arg.labels},arg.lazy);
 				else return arg.call(this);
 			}
+			eval(){return this}
 		}
-		class Dot{
-			call(){
-
+		class Dot{//'a.b'
+			constructor(name){
+				this.name=name;//:String
 			}
+			call(arg,context,stack){
+				if(arg instanceof NameSpace)return arg.labels
+			}
+			eval(){return this}
 		}
 	class Int extends Number{
 		constructor(value){
@@ -153,10 +173,10 @@
 				this.value = value;//:Number|Int
 				this.foo = arg_f;//:Lazy
 			}
-			call(arg_x){
+			call(arg_x,context,stack){
 				let ans = arg_x;
 				for(let i = 0;i<this.value;i++){
-					ans = this.foo(ans);
+					ans = this.foo.call(ans,context,stack);
 				}
 				return ans;
 			}
@@ -168,7 +188,7 @@
 			lazyExpVersion:new Lambda(new Lambda(new Lambda(1,[2,1,0]))),
 		};
 		call(arg_f){//f>x>
-			return new Part1(this,arg_f);
+			return new this.constructor.Part1(this,arg_f);
 		}
 		eval(){return this}
 	}
@@ -177,12 +197,14 @@
 			this.func=func;
 			this.args = args;
 			if(args.length>=func.length){
-				const ans = this.func(...args);
+				const ans = this.func(...args.map(v=>Lazy.toInt(v)));
+				loga(Lazy.toInt(args[0]))
 				return isNaN(ans)?Lambda.null:new Int(ans);
 			}
 			//func:(...Number[])->Number
 		}
 		args;//:Lazy[]
+		//isOperater:bool&Operator?
 		call(arg){//Int->Int-> ... Int-> Int
 			return new this.constructor(this.func,[...this.args,arg])
 		}
@@ -192,13 +214,25 @@
 		constructor(func){
 			this.func=func
 		}
+		//isOperater:bool&Operator?
 		call(arg){//Int->Int-> ... Int-> Int
 			return this.func(arg);
 		}
 		eval(){return this}
 	}
 	class List extends Array{
-
+		constructor(list){
+			this.list = list;
+		}
+		call(arg,context,stack){
+			;
+		}
+		eval(){return this}
+		list = [];
+		static get = new Func((array,index)=>
+			!(array instanceof List)?Lambda.null
+			:array.list[Lazy.toInt(index)]??Lambda.null
+		)
 	}
 	//ints
 	//f>x>f (x x)
@@ -285,13 +319,14 @@ function compile (text){
 				get parent(){return this.#parent}
 				#parent;
 				arg;//:String
-				value;//:Param?
 				type;
+				value;//:Code
+				ref;//:Param?
 			}
 			class BracketPattern extends Pattern{
 				constructor(data){super();Object.assign(this,data)}
 				parent;
-				pattern=')';//')'|']'|'}'
+				pattern='()';//'()'|'[]'|'{}'
 				list=[];//:Tree<Pattern> ; brackets only
 			}
 			class Pattern_extra{
@@ -299,18 +334,20 @@ function compile (text){
 				currentLabels;//:Map(String,Ref)
 				refs;//:Set(Pattern) & Tree(Pattern)
 				refLevel;//:Number
+				funcLevel;//:Number ; number of nested lambdas
 			};
 			class Param{
 				constructor(data){Object.assign(this,data)}
 				name;//:[String,Number] ; name of the parameter or label
 				index;//:number ; if a pattern has muli parameters it shows which one it is.
 				owner;//: Pattern & context with a {list:Array} ; is the expression that the label bellongs to
+				value;//: Lazy? ; only for assignment patterns
 			}
 			const comma = {};
 		//----
 		const match_pattern_arg = ({tree,i,pattern})=>{//'a' or '$1' 
 			let oldI=i;
-			let getObj = ()=>({i,id:tree[oldI][1],type:"argument"});
+			let getObj = ()=>({i,id:tree[oldI][1],type:"argument",pattern});
 			if(!pattern){
 				let word;
 				if((word=getWord(tree,i))=="$"){
@@ -358,8 +395,15 @@ function compile (text){
 				}
 				//if(getWord(tree,i+1)=="::"){} UNFINISHED
 			}
+			if(!pattern){
+				hasOwnContext=true;
+				options="";
+				if(getWord(tree,i)=="?"){options+="?";++i}
+				if("\\λ".includes(getWord(tree,i))){pattern="λ";type="function";list=[];++i}
+			}
 			if(!pattern){//function and assignment | 'with' block | 'use' block
 				i=oldI;
+				options="";
 				hasOwnContext=true;
 				{//assign params
 					if("([{".includes(getWord(tree,i)||undefined)){
@@ -373,7 +417,7 @@ function compile (text){
 										i++;
 									}
 									else{
-										s=foo(tree[i],i,tree)
+										s=foo(tree[i],i,tree);
 									}
 								}
 							}
@@ -397,7 +441,9 @@ function compile (text){
 				}
 			}
 			if(!pattern){
+				hasOwnContext=false;
 				i=oldI;
+				options="";
 				let match = match_pattern_arg ({tree,i});
 				if(match)return match;
 			}
@@ -433,9 +479,9 @@ function compile (text){
 						yield match;
 						continue;
 					}
-					else if("([{".includes(getWord(tree,i))){
+					else if("([{".includes(word)){
 						++i;
-						let bracket=new BracketPattern({pattern:"()",list:[],parent:context,isFirst});
+						let bracket=new BracketPattern({pattern:{"(":"()","[":"[]","{":"{}"}[word],list:[],parent:context,isFirst});
 						yield bracket;
 						context.list.push(bracket);
 						yield*getPatterns(tree[i],bracket);
@@ -460,15 +506,16 @@ function compile (text){
 			}(tree,context))],
 			context
 		};
-		function* forEachTree(context,isTree){//isTree:node->Tree?
-			for(let i=0;i<context.list.length;i++){
-				let [v,a] = [context.list[i],context];
+		function* forEachTree(context,getTree){//isTree:node->Tree?
+			const tree = getTree(context);
+			for(let i=0;i<tree.length;i++){
+				let [v,a] = [tree[i],context];
 				yield [v,i,a];
-				if(isTree(v))yield*forEachTree(v,isTree);
+				yield*forEachTree(v,getTree);
 			}
 		}
+		const forEachPattern = ()=>forEachTree(context,v=>v?.list??[]);
 		patterns.forEach = forEachPattern;
-		const forEachPattern = ()=>forEachTree(context,v=>v?.list);
 		function addRefParam(match,match_parents,param){
 			//match:Pattern
 			//match_parents:Set(BracketPattern)
@@ -476,6 +523,7 @@ function compile (text){
 			let higherParamParent = param.owner;//:Pattern
 			let argParentIndex;
 			//assume: there is not an infinite chain of Pattern.parent's
+			if(param.owner.type=="function"||param instanceof Operator)return;//functions can't form ref loops, since it is garantied that `match_parents.includes(param.owner) == true`
 			while(
 				higherParamParent
 				&& -1 == (argParentIndex = match_parents.indexOf(higherParamParent.parent))
@@ -483,6 +531,10 @@ function compile (text){
 			if(argParentIndex==-1)throw Error("compiler error");
 			if(!higherParamParent)throw Error("compiler error");
 			let higherArgParent = match_parents[argParentIndex-1];
+			if(argParentIndex==0){//e.g. '(a a=2)'
+				//UNTESTED: unknown behaviour: may not reference errors, where there should be
+				return;
+			}
 			if(!higherArgParent)throw Error("compiler error");
 			if(higherParamParent.parent!=higherArgParent.parent)throw Error("compiler error");
 			{//check for and prevent reference loops
@@ -502,10 +554,10 @@ function compile (text){
 			if(higherArgParent.refLevel<=higherParamParent.refLevel)throwError(match.id,"illegal reference","complex recursive/self reference, detected using label '"+match.arg+"'. These are not allowed",a=>Error(a));
 			higherArgParent.refs.add(higherParamParent);
 		}
-		function getParam(name,parent,parents){//parents:Pattern[]?
+		function getParam(name,parent,parents=[]){//parents:Pattern[]?
 			let param = 
 				parent?.currentLabels?.get?.(name)
-				??parent?.startLabels?.get?.(name)
+				??(p=>parents.includes(p?.owner)?undefined:p)(parent?.startLabels?.get?.(name))
 			;
 
 			//loga(match.parent.parent.startLabels.get("a"))
@@ -517,23 +569,83 @@ function compile (text){
 					:undefined
 			;
 		}
+		class Operator extends Param{
+			constructor(name,priority,foo){
+				super({name:[name,-1],priority,owner:context,value:foo});
+				this.value.isOperator = this;//:truthy
+			}
+			isParsed=false;
+			//name:[String,Number]
+			//index:Number
+			//owner:Pattern
+			//value:Lazy
+			//priority:Number
+		}
+		let maxPriority;
+		{
+			//applies to Patterns with type = "operator"
+			context.value = new Lazy;
+			let i=0;
+			const bool_true = new Lambda(new Lambda(1));
+			const bool_false = new Int(0);
+			context.startLabels= new Map([
+				//TODO: allow '!!a' -> '(! (! a))'
+				new Operator("!" ,  i,new Func((a)=>Lazy.toInt(a)==0||a==Lambda.null?bool_true:bool_false)),
+				new Operator("~" ,  i,new Calc((a)=>~a)),
+				new Operator("++",  i,new Calc((a)=>a+1)),
+				new Operator("&" ,++i,new Calc((a,b)=>a&b)),
+				new Operator("|" ,  i,new Calc((a,b)=>a|b)),
+				new Operator("^" ,  i,new Calc((a,b)=>a^b)),
+				new Operator("**",++i,new Calc((a,b)=>a**b)),
+				new Operator("*" ,++i,new Calc((a,b)=>a*b)),
+				new Operator("/" ,  i,new Calc((a,b)=>a/b)),
+				new Operator("+" ,++i,new Calc((a,b)=>a+b)),
+				new Operator("-" ,  i,new Calc((a,b)=>a-b)),
+				new Operator("==",++i,new Func((a,b)=>a==b)),
+				new Operator("&&",++i,new Lambda(new Lambda(1,0,1))),
+				new Operator("||",  i,new Lambda(new Lambda(1,1,0))),
+				new Operator("^^",  i,new Lambda(new Lambda(1,[0,bool_false,1],0))),
+			].map(v=>[v.name[0],v]));
+			maxPriority= i;
+		}
 		(function getAssignments(patterns){
+			//find definisions
 			for(let [match,i,bracketParent] of forEachPattern()){
+				match.funcLevel = match.parent?.funcLevel || 0;
 				let lastValue = match.isFirst?undefined:bracketParent[i-1];// ','
 				if(match.pattern == "="){//assignment
 					//assume: bracketParent == match.parent
-					let startLabels=bracketParent.startLabels;
-					if(!startLabels){//set up assignment properties
-						bracketParent.startLabels = startLabels = new Map();//String|Number => Param
-						bracketParent.currentLabels = new Map();
-						bracketParent.refs = new Set();//:Set(BracketPattern) where p.refs.get(x).refs.get(p) == undefined
+					{//set up assignment properties
+						bracketParent.startLabels ??= new Map();//String|Number => Param
+						bracketParent.currentLabels ??= new Map();
+						bracketParent.refs ??= new Set();//:Set(BracketPattern) where p.refs.get(x).refs.get(p) == undefined
 					}
+					let startLabels=bracketParent.startLabels;
 					let i=0;
+					match.value = new Lazy;
 					for(let param of match.params){//param:Param
 						if(!startLabels.has(param))startLabels.set(param.name[0],param);
+						param.value = match.value;
+					}
+				}
+				else if(match.type == "function"){//assume pre-fix
+					if(match.pattern==">")match.startLabels = new Map(match.params.map(v=>[v.name[0],v]));
+					match.funcLevel++;
+					match.value = new Lambda;
+				}
+				else if(match instanceof BracketPattern){
+					if(match.pattern=="()"){
+						match.value = [];
+					}
+					if(match.pattern=="[]"){
+						match.value = new List;
+					}
+					if(match.pattern=="{}"){
+						match.value = [];
 					}
 				}
 			}
+			//find and link references
 			for(let [match,i,bracketParent] of forEachPattern()){
 				let lastValue = match.isFirst?undefined:bracketParent[i-1];// ','
 				if(match.pattern == "="){//assignment
@@ -541,81 +653,94 @@ function compile (text){
 						match.parent.currentLabels.set(param.name[0],param);
 					}
 				}
-				else if(match.type == "argument"){
-					if(match.pattern == "$"){}
+				else if(match.type == "function"){//function
+					match.parent.value.push(match.value);
+				}
+				else if(match.type == "argument" || match.type == "symbol" || match.type == "number"){
+					if(match.pattern == "$"){
+						match.value = +match.arg;
+						if(typeof match.value == "number"){//lambda parameter
+							//validate value
+							if(match.value>match.funcLevel){
+								throwError(match.id,"reference","the parameter number exceeds the amount of global scopes",a=>Error(a));
+							}
+						}
+					}
 					else{
-						let {param,parents} = getParam(match.arg,match.parent,[])??{};
-						if(!param)throwError(match.id,"reference","label: '"+match.arg+"' is undefined",a=>Error(a));
-						addRefParam(match,parents,param);
-						//if(parent)
-						match.value = param;
+						let {param,parents} = getParam(match.arg,match.parent)??{};
+						if(match.type!="number"){
+							//note: number literals & other predefined values can be used as either an Int or a parameter reference
+							if(!param){
+								throwError(match.id,"reference","label: '"+match.arg+"' is undefined",a=>Error(a));
+							}
+							addRefParam(match,parents,param);
+							//assert: param != undefined
+							match.ref = param;
+							if(param.owner.type == "function"){
+								let refLevel = match.funcLevel-param.owner.funcLevel;
+								if(param.owner.options[0]=="{"){//is multi assign namespace
+									match.value = new Lazy(new Dot(param.name[0]),refLevel);
+								}
+								else if(param.owner.options[0]=="["){//is multi assign list
+									match.value = new Lazy(List.get,refLevel,new Int(param.index));
+								}
+								else{
+									match.value = refLevel;
+								}
+							}
+							else{
+								match.value = param.value;
+							}
+							if(match.value==undefined)throw Error("compiler error");
+						}
+						else if(!param){//if not overwritten && is number literal
+							match.value = new Int(+match.arg);
+						}
+					}
+					match.parent.value.push(match.value);
+				}
+				else if(match instanceof BracketPattern){
+					match.parent.value.push(match.value);
+				}
+			}
+			//handle operator priorities
+			for(let [value,i] of 
+				function*(){
+					yield [context.value,0];
+					yield*forEachTree(context.value,v=>v instanceof Array?v:[]);
+				}()
+			){
+				if(!(value instanceof Array && value.length>0))continue;
+				let list = value;
+				for(let priority=0;priority<maxPriority+1;priority++){
+					for(let i=0;i<list.length;i++){
+						let operator = list[i];
+						if(!(operator.isOperator instanceof Operator))continue;
+						if(operator.isOperator.isParsed)continue;
+						if(!(operator.isOperator.priority == priority))continue;
+						let foo = operator.func;
+						let useLeftArg = foo.length == 2;
+						if(i<list.length-1)
+						if(i>0 && !operator.isOperator.isFirst){//helps prevent parsing the same operator twice, since it wants to end up in the form '[+ a b]'
+							let newOperation = [operator];
+							let splice=list.splice(i-useLeftArg,foo.length+1);
+							newOperation.push(...(foo.length==2?[splice[0],splice[2]]:[splice[1]]));
+							list.splice(i-useLeftArg,0,newOperation);
+							i-=useLeftArg;
+							operator.isOperator.isParsed=true;
+							//'a + b' -> '(+ a b)'
+							//'! a' -> '(! a)'
+						}
 					}
 				}
 			}
+
 		}(patterns.list));
 		return context;
 	}
-	function parse(tree,list=new Lazy(),context=[],startI=0){
-		const startList = list;
-		for(let i = startI;i<tree.length;i++){
-			let value = tree[i];
-			let [word,num] = value;
-			if(word.match(/^\/\/|^\/\*|\s+/)){}//ignore comments & white space
-			else if(word.match(/[([{]/)){
-				let i1 = i+3;
-				let isMut = tree[i1]?.[0]=="?";
-				if(isMut)i1++;
-				let word1= tree[i1]?.[0]??"";
-				if(word1.match(/=|>/)){
-					if(word1.match("=")){//with'()=' or destructor assign '[]=','{}='
-					}
-					else if(word1.match("=")){//using'()>' or destructor param '[]>','{}>'
-					}
-				}
-				else {
-					switch (word){
-						case "(":list.push(parse(tree[i+1],Object.assign(new Array(),{id:num}),context));break;
-						case "[":;break;
-						case "{":;break;
-					}
-				}
-				i+=2;
-			}
-			else if(word.match(/[)\]}]/))break;
-			else if(context.indexOf(word)!=-1){
-				list.push(context.indexOf(word));
-			}
-			else if(word.match(/^["]/)){
-				list.push(word.match(/(?<=^.)[\s\S]*(?=.$)/)[0]);
-			}
-			else if(word=="λ"){
-				context = [{},...context];
-				list.push(list = new Lambda());
-			}
-			else if(word.match(/^[0-9]+$/)){
-				list.push(new Int(+word));
-			}
-			else if(word==","){
-				startList.push(list=[]);
-			}
-			else if(word=="::"){
-				throw Error("recursion setting '::' is not supported")
-				//startList.push(new RecurSetter(,list=new Lazy()));
-			}
-			else if((tree[i+1]?.[0]??"")[0]==">"){
-				context = [word,...context];
-				list.push(list = Object.assign(new Lambda(),{id:num}));
-				i++;
-			}
-		}
-		let outList = startList;
-		if(list.length == 0&&list!=startList)startList.pop();
-		if(startList.length==1)outList = outList[0];
-		return outList;
-	}
 	let regex =/[λ]|\b\w+\b|"[^"]*"|[^\s]|\s+|\/\/.*|\/\*[\s\S]*\*\//g;
 	let lines = text.split("\n");
-	const words = text.match(regex);
+	const words = text.match(regex)??[];
 	const getLineFromWord=new Map();
 	const getWordFromLine=new Map();
 	words.reduce((s,v,i)=>{
@@ -630,14 +755,19 @@ function compile (text){
 		else s.column+=v.length;
 		return s;
 	},{line:1,column:1,word:""});
-	try{//main
+	let main=()=>{
 		const tree = treeParse(words);
 		const context = parseContexts(tree);
-		return context;//parse(tree);
+		const expression = context.value;
+		return expression;//parse(tree);
+	}
+	if(TEST)return main();
+	else try{//main
+		return main();
 	}catch(error){
 		console.log(error);
+		return new Lazy;
 	}
 }
-//loga
-(compile(`a=b,b=c,c=d,d=a`))//f>(r>r x>f(r x),a>a a)`));
+loga(compile(`i=n>n(++)0,+ (3) 3`).eval())//f>(r>r x>f(r x),a>a a)`));
 //loga(Y.call(new Calc((f,n)=>n==0?1:n*f.call(n-1))).call(new Int(4)));
